57d915afe456d0f3c3a13990bd4ad3a9dd53c79c
Charan <ravivijayacharan0@gmail.com>
2025-09-09 09:10:32 +0530

    initial logic for the login api

--- urlshortner/url-login/handlers/login.go
+++ urlshortner/url-login/handlers/login.go
@@ -1,21 +1,38 @@
 package handlers
 
 import (
+	"fmt"
 	"goapp/models"
+
+	"goapp/validator"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func LoginHandler(c *fiber.Ctx) error {
 	var (
-		reqBody models.LoginRequest
-		err error
+		reqBody  models.LoginRequest
+		username string
+		err      error
 	)
 
 	if err = c.BodyParser(&reqBody); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"error": "cannot parse JSON",
 		})
+	}
+
+	// Validating the request body parameters, so that getting the expected values in each fields
+	msg := validator.ValidateRequestBody(reqBody)
+	if msg != "" {
+		fmt.Println("Invalid request body : ", msg)
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": msg})
+	}
+
+	if reqBody.Username != "" {
+		username = reqBody.Username
+	} else {
+		username = reqBody.Email
 	}
 	return nil
 }
