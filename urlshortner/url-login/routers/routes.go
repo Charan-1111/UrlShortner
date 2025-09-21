cd9d4e80fce0792abfb2a78dd4789a1cb6cd0856
Charan <ravivijayacharan0@gmail.com>
2025-09-21 22:36:57 +0530

    added cors middleware

--- urlshortner/url-login/routers/routes.go
+++ urlshortner/url-login/routers/routes.go
@@ -7,6 +7,7 @@ import (
 	"runtime/debug"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
@@ -27,8 +28,16 @@ func SetUpFiberServer() *fiber.App {
 		},
 	}))
 
-	// TODO : Need to enable the CORS middleware for the safer server
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "*",
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH",
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		ExposeHeaders:    "Content-Length",
+		AllowCredentials: true,
+		MaxAge:           3600,
+	}))
 
+	// Setting up the routes
 	apiGroup := app.Group("/urlshortner")
 	apiGroup.Get("/health", func(c *fiber.Ctx) error {
 		return c.SendString("URL Shortener is healthy!")
